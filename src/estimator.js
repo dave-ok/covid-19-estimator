053fb819d69c03bcf386f48f5b2711fd6c460410
Dave <callremz@gmail.com>
2020-04-08 05:53:29 +0100

    fix more lint errors

--- src/estimator.js
+++ src/estimator.js
@@ -1,22 +1,13 @@
-const covid19ImpactEstimator = (data) => {
-  const impactObj = new ImpactEstimator(data, 10);
-  const severeImpactObj = new ImpactEstimator(data, 50);
-  return {
-    data,
-    impact: impactObj.getEstimate(),
-    severeImpact: severeImpactObj.getEstimate()
-  }
-}
-
 class ImpactEstimator {
   constructor({
-    region, 
-    periodType, 
-    timeToElapse, 
-    reportedCases, 
-    population, 
-    totalHospitalBeds},
-    severityFactor){
+    region,
+    periodType,
+    timeToElapse,
+    reportedCases,
+    population,
+    totalHospitalBeds
+  },
+  severityFactor) {
     this.region = region;
     this.periodType = periodType;
     this.timeToElapse = timeToElapse;
@@ -24,38 +15,46 @@ class ImpactEstimator {
     this.population = population;
     this.totalHospitalBeds = totalHospitalBeds;
     this.severityFactor = severityFactor;
-  }   
-  
+  }
+
   periodInDays() {
     switch (this.periodType) {
-      case "days": {
-        return this.timeToElapse;
-      }
-      case "weeks": {
+      case 'weeks': {
         return this.timeToElapse * 7;
       }
-      case "months": {
+      case 'months': {
         return this.timeToElapse * 30;
       }
+      default: {
+        return this.timeToElapse;
+      }
     }
-  } 
+  }
 
   get currentlyInfected() {
     return this.reportedCases * this.severityFactor;
   }
 
   get infectionsByRequestedTime() {
-    const powerFactor = Math.floor(this.periodInDays/3);
-    return this.currentlyInfected * 2**powerFactor;
+    const powerFactor = Math.floor(this.periodInDays / 3);
+    return this.currentlyInfected * 2 ** powerFactor;
   }
 
   getEstimate() {
     return {
-      
-    }
-
+      infectionsByRequestedTime: this.infectionsByRequestedTime
+    };
   }
 }
+const covid19ImpactEstimator = (data) => {
+  const impactObj = new ImpactEstimator(data, 10);
+  const severeImpactObj = new ImpactEstimator(data, 50);
+  return {
+    data,
+    impact: impactObj.getEstimate(),
+    severeImpact: severeImpactObj.getEstimate()
+  };
+};
+
 
 export default covid19ImpactEstimator;
-
