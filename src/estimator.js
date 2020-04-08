40a54b1177044d1f6341074138ca11ff8309fcae
Dave <callremz@gmail.com>
2020-04-08 05:20:34 +0100

    create ImpactEstimator class

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,38 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const impactObj = new ImpactEstimator(data, 10);
+  const severeImpactObj = new ImpactEstimator(data, 50);
+  return {
+    data,
+    impact: impactObj.getEstimate(),
+    severeImpact: severeImpactObj.getEstimate()
+  }
+}
+
+class ImpactEstimator {
+  constructor({
+    region, 
+    periodType, 
+    timeToElapse, 
+    reportedCases, 
+    population, 
+    totalHospitalBeds},
+    severityFactor){
+    this.region = region;
+    this.periodType = periodType;
+    this.timeToElapse = timeToElapse;
+    this.reportedCases = reportedCases;
+    this.population = population;
+    this.totalHospitalBeds = totalHospitalBeds;
+    this.severityFactor = severityFactor;
+  }    
+
+  getEstimate() {
+    return {
+      
+    }
+
+  }
+}
 
 export default covid19ImpactEstimator;
+
