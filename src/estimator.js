bf67b79db23745ccde97ee908bfc7bf1438591de
Dave <callremz@gmail.com>
2020-04-08 05:21:54 +0100

    complete challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -24,7 +24,30 @@ class ImpactEstimator {
     this.population = population;
     this.totalHospitalBeds = totalHospitalBeds;
     this.severityFactor = severityFactor;
-  }    
+  }   
+  
+  periodInDays = () => {
+    switch (this.periodType) {
+      case "days": {
+        return this.timeToElapse;
+      }
+      case "weeks": {
+        return this.timeToElapse * 7;
+      }
+      case "months": {
+        return this.timeToElapse * 30;
+      }
+    }
+  } 
+
+  get currentlyInfected() {
+    return this.reportedCases * this.severityFactor;
+  }
+
+  get infectionsByRequestedTime() {
+    const powerFactor = Math.floor(this.periodInDays/3);
+    return this.currentlyInfected * 2**powerFactor;
+  }
 
   getEstimate() {
     return {
