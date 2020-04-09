69afebe3765a3a247369b7ae983d62b0d94ec53b
Dave <callremz@gmail.com>
2020-04-09 23:49:38 +0100

    checking

--- src/api/utils/requestLogger.js
+++ src/api/utils/requestLogger.js
@@ -1,9 +1,14 @@
 import fs from 'fs';
 import path from 'path';
 
-const writeLog = (time, reqType, endpoint, resCode) => {
+const writeLog = (time, reqType, endpoint, resCode, reqBody) => {
   const resMessage = `\n${reqType}\t\t${endpoint.padEnd(25)}\t\t${resCode}\t\t${time.toString().padStart(12)} ms`;
   fs.appendFile(path.join(process.cwd(), '/src/requests.log'), resMessage, (error) => {
+    if (error) {
+      throw error;
+    }
+  });
+  fs.appendFile(path.join(process.cwd(), '/src/requests.log'), reqBody, (error) => {
     if (error) {
       throw error;
     }
@@ -19,7 +24,7 @@ export default (req, res, next) => {
     const msTime = totalTime[0] * 1000 + totalTime[1] / 1e6;
 
     // log to file
-    writeLog(msTime, req.method, req.originalUrl, res.statusCode);
+    writeLog(msTime, req.method, req.originalUrl, res.statusCode, req.body.toString());
   });
 
   next();
