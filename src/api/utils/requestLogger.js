9379ef65e08493036a75fbca60284e475ebc1a0a
Dave <callremz@gmail.com>
2020-04-12 15:32:50 +0100

    modify log file format

--- src/api/utils/requestLogger.js
+++ src/api/utils/requestLogger.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 const writeLog = (time, reqType, endpoint, resCode) => {
-  const resMessage = `${reqType}\t\t${endpoint.padEnd(25)}\t\t${resCode}\t\t${time.toString().padStart(12)} ms\n`;
+  const resMessage = `${reqType.padEnd(7)}${endpoint.padEnd(25)}\t\t${resCode}\t\t${time.toString().padStart(4)}ms\n`;
   fs.appendFile(path.join(process.cwd(), '/src/requests.log'), resMessage, (error) => {
     if (error) {
       throw error;
@@ -16,7 +16,7 @@ export default (req, res, next) => {
 
   res.on('finish', () => {
     const totalTime = process.hrtime(reqTime);
-    const msTime = totalTime[0] * 1000 + totalTime[1] / 1e6;
+    const msTime = Math.round(totalTime[0] * 1000 + totalTime[1] / 1e6);
 
     // log to file
     writeLog(msTime, req.method, req.originalUrl, res.statusCode);
