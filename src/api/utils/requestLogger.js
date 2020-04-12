fff35b6be871a562e4b6a1adf5a6e1847afefbf9
Dave <callremz@gmail.com>
2020-04-12 23:42:16 +0100

    last minute mod

--- src/api/utils/requestLogger.js
+++ src/api/utils/requestLogger.js
@@ -1,8 +1,10 @@
 import fs from 'fs';
 import path from 'path';
 
+const url = require('url');
+
 const writeLog = (time, reqType, endpoint, resCode) => {
-  const resMessage = `${reqType.padEnd(7)}${endpoint.padEnd(25)}\t\t${resCode}\t\t${time.toString().padStart(4)}ms\n`;
+  const resMessage = `${reqType.padEnd(7)}${endpoint.padEnd(25)}\t\t${resCode}\t\t${time.padStart(4)}ms\n`;
   fs.appendFile(path.join(process.cwd(), '/src/requests.log'), resMessage, (error) => {
     if (error) {
       throw error;
@@ -16,10 +18,12 @@ export default (req, res, next) => {
 
   res.on('finish', () => {
     const totalTime = process.hrtime(reqTime);
-    const msTime = Math.round(totalTime[0] * 1000 + totalTime[1] / 1e6);
+    const msTime = Math.round(totalTime[0] * 1000 + totalTime[1] / 1e6).toString().padStart(2, '0');
 
-    // log to file
-    writeLog(msTime, req.method, req.originalUrl, res.statusCode);
+    // log to file skip not founds
+    if (res.statusCode !== 404) {
+      writeLog(msTime, req.method, url.parse(req.originalUrl).pathname, res.statusCode);
+    }
   });
 
   next();
